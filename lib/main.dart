b82a1c70903f61bd6fc167b3c5e35bfbdabe1a4f
KristinBrGultom <kristingultom190819@gmail.com>
2022-03-29 10:03:02 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Konverter Suhu, Kristin Br Gultom 2031710107"),
+          title: const Text("Konverter Suhu"),
         ),
         body: Container(
           margin: const EdgeInsets.all(8),
